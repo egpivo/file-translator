a8fe69bd309fc60410d4f2109f8c24b6f3aff593
Joseph (Wen-Ting) Wang <egpivo@gmail.com>
2025-01-26 10:06:58 +0400

    Update config.py

--- file_translator/config.py
+++ file_translator/config.py
@@ -3,7 +3,5 @@ import os
 SIZE_THRESHOLD = 5 * 1024 * 1024
 CACHE_DIR = os.path.join(os.getcwd(), "cache")
 
-TRANSLATION_API_URL = (
-    "https://vitruvian-translation-worker-dev.presight.ai/api/v1/translate"
-)
+TRANSLATION_API_URL = # your own LLM translator API
 EXCEL_MEDIA_TYPE = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
